da5c402775cbeca6e13c5681aeb49de8686f0989
PhuPhuoc <blessforwork@gmail.com>
2023-08-04 00:26:11 +0700

    phuoc commit 3

--- src/components/navbar/HeaderComponents.jsx
+++ src/components/navbar/HeaderComponents.jsx
@@ -1,33 +1,83 @@
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-const { Header, Content, Footer } = Layout;
+import { Menu } from 'antd';
+import { AppstoreOutlined, HomeOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Col, Row } from 'antd';
+import { useState } from 'react';
+import logo from "../../assets/Logo.png";
+import "./header.css";
+
+const items = [
+    {
+        label: 'Home',
+        key: 'Home',
+        icon: <HomeOutlined />,
+
+    },
+    {
+        label: 'About',
+        key: 'About',
+        icon: <AppstoreOutlined />,
+    },
+    {
+        label: 'Services',
+        key: 'Services',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                type: 'group',
+                label: 'Service 1',
+                children: [
+                    {
+                        label: 'Option 1',
+                        key: 'setting:1',
+                    },
+                    {
+                        label: 'Option 2',
+                        key: 'setting:2',
+                    },
+                ],
+            },
+            {
+                type: 'group',
+                label: 'Service 2',
+                children: [
+                    {
+                        label: 'Option 3',
+                        key: 'setting:3',
+                    },
+                    {
+                        label: 'Option 4',
+                        key: 'setting:4',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        label: 'Contact',
+        key: 'Contact',
+        icon: <MailOutlined />,
+    },
+    {
+        label: 'Join Us',
+        key: 'joinUs',
+    },
+];
 
 const HeaderComponents = () => {
+    const [current, setCurrent] = useState('Home');
+    const onClick = (e) => {
+        setCurrent(e.key);
+    }
     return (
         <>
-            <Layout>
-                <Header
-                    style={{
-                        position: 'sticky',
-                        top: 0,
-                        zIndex: 1,
-                        width: '100%',
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}
-                >
-                    <div className="demo-logo" />
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                        items={new Array(3).fill(null).map((_, index) => ({
-                            key: String(index + 1),
-                            label: `nav ${index + 1}`,
-                        }))}
-                    />
-                </Header>
-            </Layout>
-
+            <Row style={{}}>
+                <Col span={12}>
+                    <img src={logo} alt="" style={{marginLeft: '170px', marginTop: '12px'}}/>
+                </Col>
+                <Col span={12}>
+                    <Menu style={{marginLeft: '190px'}} onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+                </Col>
+            </Row>
         </>
     );
 };
